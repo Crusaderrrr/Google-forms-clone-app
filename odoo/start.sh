a912885f4319a1a183852207b93e49a89cd1207d
Nikita Bulash <bulash2006@gmail.com>
2025-07-15 19:33:46 +0100

    timeout added

--- odoo/start.sh
+++ odoo/start.sh
@@ -3,12 +3,7 @@ cd /tmp
 git clone --depth 1 --branch 17.0 https://github.com/odoo/odoo.git odoo-source
 pip install -r odoo-source/requirements.txt
 
-# Add explicit host binding and longer timeout
-python odoo-source/odoo-bin \
-    --config=/app/odoo.conf \
-    --http-port=$PORT \
-    --http-interface=0.0.0.0 \
-    --workers=0 \
-    --max-cron-threads=0 \
-    --limit-memory-soft=536870912 \
-    --limit-memory-hard=671088640
+# Start Odoo in background and wait
+python odoo-source/odoo-bin --config=/app/odoo.conf --http-port=$PORT --http-interface=0.0.0.0 &
+sleep 30
+wait
