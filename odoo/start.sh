2b53ef3070035248f4f99b1ea728aa5ec72facfc
Nikita Bulash <bulash2006@gmail.com>
2025-07-15 19:31:06 +0100

    start fixed

--- odoo/start.sh
+++ odoo/start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-pip install --upgrade pip setuptools wheel
 cd /tmp
 git clone --depth 1 --branch 17.0 https://github.com/odoo/odoo.git odoo-source
 pip install -r odoo-source/requirements.txt
-python odoo-source/odoo-bin --config=/app/odoo.conf --http-port=$PORT --http-interface=0.0.0.0
+
+# Add explicit host binding and longer timeout
+python odoo-source/odoo-bin \
+    --config=/app/odoo.conf \
+    --http-port=$PORT \
+    --http-interface=0.0.0.0 \
+    --workers=0 \
+    --max-cron-threads=0 \
+    --limit-memory-soft=536870912 \
+    --limit-memory-hard=671088640
