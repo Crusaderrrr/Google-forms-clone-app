8b73f6d5ae2fad7ecd1b13ecbba7a0a4e202637b
Nikita Bulash <bulash2006@gmail.com>
2025-07-15 18:17:58 +0100

    lastest commit

--- odoo/start.sh
+++ odoo/start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-odoo --config=/app/odoo.conf --http-port=$PORT
+git clone --depth 1 --branch 17.0 https://github.com/odoo/odoo.git /app/odoo-source
+pip install -r /app/odoo-source/requirements.txt
+python /app/odoo-source/odoo-bin --config=/app/odoo.conf --http-port=$PORT
