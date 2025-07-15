a34f424b97a0f6667525c091aeb3db0e0f61ebd5
Nikita Bulash <bulash2006@gmail.com>
2025-07-15 18:27:44 +0100

    fixed

--- odoo/start.sh
+++ odoo/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd ~
+pip install --upgrade pip setuptools wheel
+cd /tmp
 git clone --depth 1 --branch 17.0 https://github.com/odoo/odoo.git odoo-source
-pip install -r ~/odoo-source/requirements.txt
-python ~/odoo-source/odoo-bin --config=/app/odoo.conf --http-port=$PORT
+pip install -r odoo-source/requirements.txt
+python odoo-source/odoo-bin --config=/app/odoo.conf --http-port=$PORT
