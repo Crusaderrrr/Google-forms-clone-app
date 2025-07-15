f7b03b9ff271aefb7ef7516cea216fa1bebf3ac3
Nikita Bulash <bulash2006@gmail.com>
2025-07-15 19:40:44 +0100

    runtime added

--- odoo/start.sh
+++ odoo/start.sh
@@ -2,8 +2,6 @@
 cd /tmp
 git clone --depth 1 --branch 17.0 https://github.com/odoo/odoo.git odoo-source
 pip install -r odoo-source/requirements.txt
-
-# Start Odoo in background and wait
 python odoo-source/odoo-bin --config=/app/odoo.conf --http-port=$PORT --http-interface=0.0.0.0 &
 sleep 30
 wait
