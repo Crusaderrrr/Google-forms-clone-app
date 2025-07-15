a0860b1beace6193e2012a2dd183ceb20f2e64d0
Nikita <143893066+Crusaderrrr@users.noreply.github.com>
2025-07-15 20:08:52 +0100

    Update start.sh

--- odoo/start.sh
+++ odoo/start.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
+rm -rf /tmp/odoo-source
 cd /tmp
 git clone --depth 1 --branch 17.0 https://github.com/odoo/odoo.git odoo-source
-pip install -r odoo-source/requirements.txt --ignore-installed gevent
-
-python odoo-source/odoo-bin \
-    --config=/app/odoo.conf \
-    --workers=1 \
-    --max-cron-threads=0 \
-    --http-port=$PORT \
-    --http-interface=0.0.0.0 \
-    --limit-memory-soft=536870912 \
-    --limit-memory-hard=671088640 \
-    --no-database-list
+pip install -r odoo-source/requirements.txt
+python odoo-source/odoo-bin --config=/app/odoo.conf --http-port=$PORT --http-interface=0.0.0.0
